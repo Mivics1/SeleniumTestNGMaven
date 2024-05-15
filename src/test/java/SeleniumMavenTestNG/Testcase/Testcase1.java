cbc632f89948c65aefd51d4efed55e8a1c0833a3
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-05-15 14:31:32 +0100

    Added my files

---
+++ src/test/java/SeleniumMavenTestNG/Testcase/Testcase1.java
@@ -0,0 +1,36 @@
+package SeleniumMavenTestNG.Testcase;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class Testcase1 {
+	public static WebDriver driver;
+	@BeforeTest
+	public void LaunchBrowser() {
+		System.setProperty("webdriver.edge.driver","C:\\\\Selenium-drivers\\\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test (priority = 1)
+	public void checkGoogle() {
+		driver.get("https://google.com");
+		Assert.assertEquals("Google", driver.getTitle());
+	}
+	@Test (priority = 2)
+	public void checkYahoo() {
+		driver.get("https://yahoo.com");
+		Assert.assertEquals("https://www.yahoo.com/",driver.getCurrentUrl());
+	}
+	
+	@AfterTest
+	public void CloseBrowser() {
+		driver.quit();
+	}
+
+}
